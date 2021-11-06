a1240c6a30076ec23c31402eb5ca79ef16d32b24
meatbag7 <10795603+meatbag7@users.noreply.github.com>
2021-11-06 06:12:46 +0100

    HomePage first commit

--- src/main/java/PageObject.java
+++ src/main/java/PageObject.java
@@ -1,11 +1,18 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class PageObject {
     protected WebDriver driver;
 
+    @FindBy(xpath = "/html/body/div/div[1]/header/div[3]/div/div/div[6]/ul/li[1]/a")
+    private WebElement woman;
+
     public PageObject(WebDriver driver){
         this.driver=driver;
         PageFactory.initElements(driver,this);
     }
+
+    public void pressNavButton(){this.woman.click();}
 }
