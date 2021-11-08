948d089b16c52f7f8eff74434013ca345450e1bb
meatbag7 <10795603+meatbag7@users.noreply.github.com>
2021-11-08 03:50:37 +0100

    Fixed driver window size.

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -14,6 +14,7 @@ public class SmokeTest {
 
     @Test(testName = "End to end")
     public static void endToEnd(){
+        driver.manage().window().maximize();
         driver.get(Utils.HOME_URL);
     }
 
