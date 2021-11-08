a28508e13f20db71ac749f045267269fcc3c1c8f
meatbag7 <10795603+meatbag7@users.noreply.github.com>
2021-11-08 02:10:27 +0100

    HomePage object finished

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,11 +1,15 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends PageObject{
 
+    @FindBy(xpath = "/html/body/div/div[1]/header/div[3]/div/div/div[6]/ul/li[1]/a")
+    private WebElement woman;
 
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
-
+    public void pressNavButton(){this.woman.click();}
 }
--- src/main/java/PageObject.java
+++ src/main/java/PageObject.java
@@ -6,13 +6,12 @@ import org.openqa.selenium.support.PageFactory;
 public class PageObject {
     protected WebDriver driver;
 
-    @FindBy(xpath = "/html/body/div/div[1]/header/div[3]/div/div/div[6]/ul/li[1]/a")
-    private WebElement woman;
+
 
     public PageObject(WebDriver driver){
         this.driver=driver;
         PageFactory.initElements(driver,this);
     }
 
-    public void pressNavButton(){this.woman.click();}
+
 }
